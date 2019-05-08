fa20d60545a9df6cd10ceccd6c67d9829dcf9e6d
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-08 13:23:25 -0700

    POST requests for /api/auth/register and /api/auth/login endpoints. Token created upon login success.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -13,17 +13,79 @@ const usersDb = require("../users/users-model.js");
 
 // Register
 authRouter.post("/register", (req, res) => {
+  let user = req.body;
 
+  if (!user.username || !user.password || !user.department) {
+    res
+      .status(400)
+      .json({
+        error:
+          "Please include a username, password, and department to register."
+      });
+  } else {
+    // hash the password
+    const hash = bcrypt.hashSync(user.password, 10); // 2^n rounds
+    user.password = hash;
+
+    usersDb
+      .add(user)
+      .then(registered => {
+        res.status(201).json(registered);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({
+            error: "There was an error registering this user to the database."
+          });
+      });
+  }
 });
-
 
 // Login
 authRouter.post("/login", (req, res) => {
+  let { username, password } = req.body;
 
+  usersDb
+    .findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user); // <<< token
+        res
+          .status(200)
+          .json({ message: `Login success! Welcome ${user.username}!`, user, token }); // <<< token
+      } else {
+        res
+          .status(401)
+          .json({ error: "You shall not pass!" });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "Uh oh! There was an error logging you in." });
+    });
 });
 
-function generateToken(user) {
-    
-}
+
+// Token
+function generateToken(user) { // <<< token
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        department: user.department
+    };
+
+    secret = secrets.jwtSecret;
+
+    const options = {
+        expiresIn: "5h"
+    };
+
+    return jwt.sign(payload, secret, options);
+};
+
+// Logout
 
 module.exports = authRouter;
