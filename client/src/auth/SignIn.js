3fe89c9e0da852a87ad4acf1f0cceb2d38c0fea5
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-09 12:48:45 -0700

    SignIn component completed.

--- client/src/auth/SignIn.js
+++ client/src/auth/SignIn.js
@@ -2,12 +2,50 @@ import React from "react";
 import axios from "axios";
 
 class SignIn extends React.Component {
-  state = {};
+  state = {
+      username: "",
+      password: "",
+  };
+
+  handleChanges = e => {
+      const { id, value } = e.target;
+      this.setState({ [id]: value });
+  };
+
+  submitForm = e => {
+      e.preventDefault();
+      const endpoint = "http://localhost:5000/api/auth/login";
+
+      axios.post(endpoint, this.state).then(res => {
+          localStorage.setItem('jwt', res.data.token);
+          this.props.history.push("/users"); // make sure App exported withRouter
+      }).catch(err => {
+          console.error('Login Error', err);
+      });
+
+  }
 
   render() {
     return (
         <div>
             <h2>Sign In</h2>
+            <form onSubmit={this.submitForm}>
+                <input
+                id="username"
+                type="text"
+                value={this.state.username}
+                onChange={this.handleChanges}
+                placeholder="username"
+                />
+                <input
+                id="password"
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChanges}
+                placeholder="password"
+                />
+                <button>Sign In</button>
+            </form>
         </div>
     );
   }
